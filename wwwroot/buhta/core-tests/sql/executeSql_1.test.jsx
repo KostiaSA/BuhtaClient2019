59d6aff1f85a6a8a7694631ffbd9840ea6556b80
KostiaSA <sonyk795>
2017-12-20 19:33:36 +0300

    --

--- wwwroot/buhta/core-tests/sql/executeSql_1.test.jsx
+++ wwwroot/buhta/core-tests/sql/executeSql_1.test.jsx
@@ -2,13 +2,52 @@ class Test extends buhta.test.BaseTest {
 
     async mssql() {
         let paramsObj = {
-            value1: "строка777"
+            value1: "строка777",
+            value2: -1,
+            value3: moment(),
+
         };
 
         let res = await buhta.sql.executeSql("buhta/core-tests/sql/executeSql_1.test", paramsObj, "testmssql");
         console.log(res[0].rows);
 
         assert.equal(res[0].rows[0].value1, paramsObj.value1);
+        assert.equal(res[1].rows[0].value1, paramsObj.value2);
+        assert.equal(res[2].rows[0].value1.toString(), paramsObj.value3.toString());
+
+    }
+
+    async mysql() {
+        let paramsObj = {
+            value1: "строка777",
+            value2: -1,
+            value3: moment(),
+
+        };
+
+        let res = await buhta.sql.executeSql("buhta/core-tests/sql/executeSql_1.test", paramsObj, "testmysql");
+        console.log(res[0].rows);
+
+        assert.equal(res[0].rows[0].value1, paramsObj.value1);
+        assert.equal(res[1].rows[0].value1, paramsObj.value2);
+        assert.equal(res[2].rows[0].value1.toString(), paramsObj.value3.toString());
+
+    }
+
+    async postgres() {
+        let paramsObj = {
+            value1: "строка777",
+            value2: -1,
+            value3: moment(),
+
+        };
+
+        let res = await buhta.sql.executeSql("buhta/core-tests/sql/executeSql_1.test", paramsObj, "testpostgres");
+        console.log(res[0].rows);
+
+        assert.equal(res[0].rows[0].value1, paramsObj.value1);
+        assert.equal(res[1].rows[0].value1, paramsObj.value2);
+        assert.equal(res[2].rows[0].value1.toString(), paramsObj.value3.toString());
 
     }
 
--- wwwroot/buhta/core-tests/sql/executeSql_1.test.sql
+++ wwwroot/buhta/core-tests/sql/executeSql_1.test.sql
@@ -1,1 +1,8 @@
-SELECT {{value1}} as value1 --3390--03
+SELECT {{value1}} as value1 /*3333333*/
+-- wewer
+go
+SELECT {{value2}} as value1 -- 6867
+-- eeeeeeeeeeee
+go
+SELECT {{value3}} as value1; -- коммент 6867
+/*3333333*/
