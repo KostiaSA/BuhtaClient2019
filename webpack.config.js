c2cf4392c541748410620a14c532c7e1088d07ee
KostiaSA <sonyk795>
2017-12-21 17:46:26 +0300

    --

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     //     __filename: true,
     // },
     // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+    devtool: "cheap-module-source-map",
 
     // resolve: {
     //     // Add '.ts' and '.tsx' as resolvable extensions.
